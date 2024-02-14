9a35334f19afc2e5db4c96a02ca0878d9e6ae403
Mentat <jcguzmanr@gmail.com>
2024-02-14 08:36:41 -0500

    Initial Commit

---
+++ TipKitTest/TipKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitTestApp.swift
+//  TipKitTest
+//
+//  Created by jcguzmanr on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TipKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
